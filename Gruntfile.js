c689054bbc9ff66dcf21d66788874314902c9c4b
thanh.np <thanh.np@sutrixmedia.com>
2015-01-12 22:03:42 +0700

    - Init some files - Add Grunt, Watch, Connect, JsHint, QUnit

---
+++ Gruntfile.js
@@ -0,0 +1,41 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    jshint: {
+      all: ['app/**/*.js'],
+      options: {
+        ignores: ['app/test/**/*.js']
+      }
+    },
+    connect: {
+      server: {
+        options: {
+          port: 9000,
+          hostname: '*',
+          base: 'app'
+        }
+      }
+    },
+    watch: {
+      scripts: {
+        files: ['app/**/*.js'],
+        tasks: ['jshint']
+      }
+    }
+  });
+
+  //Run server
+  grunt.loadNpmTasks('grunt-contrib-connect');
+
+  // Load jshint
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  //watch files
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Default task(s).
+  grunt.registerTask('default', ['connect', 'jshint', 'watch']);
+
+};
